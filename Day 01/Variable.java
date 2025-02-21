8dfd0c428c9f6d1c16bbf591870352e519b868b2
myFirst <ytrajput68@gmail.com>
2025-02-21 16:25:58 +0530

    Day 01

---
+++ Day 01/Variable.java
@@ -0,0 +1,18 @@
+
+public class Variable {
+
+    public static void main(String[] agrs) {
+        int G = 86;
+        int S = 72;
+        System.out.println(S);
+        System.out.println(G);
+        System.out.println("The sum of G and S is: " + (G + S));
+        // float data type
+        float myFloat = 8.6f;
+        System.out.println(myFloat);
+        // stringn define
+        String mydoll = "love you";
+        System.out.println(mydoll);
+
+    }
+}
