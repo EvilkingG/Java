77a38d972f9378ac87d8399b1eba7da1e4af3337
myFirst <ytrajput68@gmail.com>
2025-02-23 15:13:59 +0530

    Day 04

---
+++ Day 04/OrderOfOperator.java
@@ -0,0 +1,7 @@
+public class OrderOfOperator {
+    public static void main(String[] args) {
+        System.out.println(86-72); // 14
+        System.out.println(5 + 2 * 3); // 11
+        System.out.println(5 + 2 * 3 - 1); // 10
+    }
+}
---
+++ Day 04/sh.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class sh {
+    public static void main(String[] args) {
+        int S = 72;
+        Scanner input = new Scanner(System.in);
+        // Taking input from user 5 Numbers
+        int G1 = input.nextInt();
+        S = S + G1;
+        System.out.println("Sum : " + S);
+        int G2 = input.nextInt();
+        S = S - G2;
+        System.out.println("Sub : " + S);
+        int G3 = input.nextInt();
+        S = S * G3;
+        System.out.print("Multiply : " + S);
+        int G4 = input.nextInt();
+        S = S / G4;
+        System.out.print("Divide : " + S);
+        int G5 = input.nextInt();
+        S = S % G5;
+        System.out.print("Mod : " + S);
+
+    }
+}
